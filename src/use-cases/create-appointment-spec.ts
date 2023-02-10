cb12effda727a95bae1b40e8744c4f6f9719d203
CleitonBrito <cleytonbritto3003@gmail.com>
2023-02-10 00:45:35 -0300

    create test cannot create appointment overlapping dates

--- src/use-cases/create-appointment-spec.ts
+++ src/use-cases/create-appointment-spec.ts
@@ -20,4 +20,48 @@ describe('Create Appointment', () =>{
             endsAt
         })).resolves.toBeInstanceOf(Appointment)
     })
+
+    it('should not be able to create an appointment with overlapping dates', async () => {
+        const startsAt = getFutureDate('2022-08-10')
+        const endsAt = getFutureDate('2022-08-15')
+
+        const appointmentsRepository = new inMemoryAppointmentsRepository()
+        const createAppointment = new CreateAppointment(
+            appointmentsRepository
+        )
+
+        await createAppointment.execute({
+            customer: 'John Doe',
+            startsAt,
+            endsAt
+        })
+
+        expect(createAppointment.execute({
+            customer: 'John Doe',
+            startsAt: getFutureDate('2022-08-14'),
+            endsAt: getFutureDate('2022-08-18')
+
+        })).rejects.toBeInstanceOf(Error)
+
+        expect(createAppointment.execute({
+            customer: 'John Doe',
+            startsAt: getFutureDate('2022-08-08'),
+            endsAt: getFutureDate('2022-08-12')
+
+        })).rejects.toBeInstanceOf(Error)
+
+        expect(createAppointment.execute({
+            customer: 'John Doe',
+            startsAt: getFutureDate('2022-08-08'),
+            endsAt: getFutureDate('2022-08-17')
+
+        })).rejects.toBeInstanceOf(Error)
+
+        expect(createAppointment.execute({
+            customer: 'John Doe',
+            startsAt: getFutureDate('2022-08-18'),
+            endsAt: getFutureDate('2022-08-20')
+
+        })).rejects.toBeInstanceOf(Error)
+    })
 })
